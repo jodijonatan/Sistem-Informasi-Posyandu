6fd0e1fe6a62fc63bbeda9847e5001e1f56fff3f
Jodi Jonatan <jdi102350@gmail.com>
2025-10-07 08:59:32 +0700

    update bidan class

--- src/main/Bidan.java
+++ src/main/Bidan.java
@@ -6,7 +6,7 @@ class Bidan extends PetugasPosyandu {
     public void setNomorLisensi(String nomor) {
         this.nomorLisensi = nomor;
     }
-
+    
     public void periksaIbuHamil(IbuHamil ibu) {
         System.out.println(this.nama + ", seorang Bidan dengan nomor lisensi " + this.nomorLisensi + ", melakukan pemeriksaan terhadap Ibu " + ibu.nama + ".");
     }
